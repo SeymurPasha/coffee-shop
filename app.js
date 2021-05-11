a63cdad6d9ae548f9ceb5629d556bd66e78c7fdc
SeymurPasha <pashayevseymur42@gmail.com>
2021-05-11 16:20:50 +0400

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+
+window.addEventListener('load', () => {
+    
+document.querySelector('.cup').classList.add('hide') 
+document.querySelector('.cup-handle').classList.add('hide') 
+    
+})
+
+
+const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+
+
+      let bars = document.getElementById('hamburger')
+      let navbar = document.querySelector('.navbar')
+      console.log(navbar);
+      bars.addEventListener('click',  () => {
+       navbar.classList.toggle('active')
+      })
